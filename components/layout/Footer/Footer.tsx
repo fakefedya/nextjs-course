d81f76cb104ce19d06c560e43ad3bce17a8b4546
Фёдор Полевой <polevoy@ligat.su>
2025-12-15 16:53:34 +0300

    Верстка Footer

--- components/layout/Footer/Footer.tsx
+++ components/layout/Footer/Footer.tsx
@@ -1,3 +1,5 @@
+import styles from './Footer.module.css'
+
 interface FooterProps {
 	className?: string
 }
@@ -5,7 +7,19 @@ interface FooterProps {
 export function Footer({ ...props }: FooterProps) {
 	return (
 		<footer {...props}>
-			<h1>Footer</h1>
+			<div className={styles.footerWrapper}>
+				<div className={styles.rights}>
+					OwlTop &copy; 2020 - {new Date().getFullYear()} Все права защищены
+				</div>
+				<div className={styles.links}>
+					<a href='/' target='_blank'>
+						Пользовательское соглашение
+					</a>
+					<a href='/' target='_blank'>
+						Политика конфиденциальности
+					</a>
+				</div>
+			</div>
 		</footer>
 	)
 }
