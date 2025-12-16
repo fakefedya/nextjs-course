c881d1e542521d8474f9c75c41c86a34f85970c0
Фёдор Полевой <polevoy@ligat.su>
2025-12-16 16:48:38 +0300

    generateStaticParams

--- app/(site)/products/[alias]/page.tsx
+++ app/(site)/products/[alias]/page.tsx
@@ -2,9 +2,19 @@ import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
 import { getPage } from '@/lib/page'
+import { getMenu } from '@/lib/menu'
 
 export const metadata: Metadata = {
 	title: 'Страница продукта',
+}
+
+export async function generateStaticParams() {
+	const menu = await getMenu(0)
+	return menu.flatMap((item) =>
+		item.pages.map((page) => ({
+			alias: page.alias,
+		})),
+	)
 }
 
 export default async function Product({
