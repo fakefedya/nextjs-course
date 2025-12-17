36f3a96f5211f01be8050c94909770fb0fabbd33
Фёдор Полевой <polevoy@ligat.su>
2025-12-17 16:03:45 +0300

    Попытка в API сайдбара

--- components/layout/Menu/Menu.tsx
+++ components/layout/Menu/Menu.tsx
@@ -1,13 +1,23 @@
+import { TopLevelCategory } from '@/interfaces/page.interface'
 import { getMenu } from '@/lib/menu'
 
 export async function Menu() {
-	const menu = await getMenu(0)
+	const firstCategory = TopLevelCategory.Products
+	console.log(firstCategory)
+	const menu = await getMenu(firstCategory)
+
+	console.log(menu)
 
 	return (
 		<div>
 			<ul>
 				{menu.map((el) => (
-					<li key={el._id.secondCategory}>{el._id.secondCategory}</li>
+					<li key={el._id.secondCategory}>
+						{el._id.secondCategory}
+						{el.pages.map((page) => (
+							<span>{page.category}</span>
+						))}
+					</li>
 				))}
 			</ul>
 		</div>
