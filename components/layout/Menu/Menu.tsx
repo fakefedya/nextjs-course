fe3373aa2ebcb59504e483e07b6634ecf85937c7
Fedya <lilfedya@yandex.ru>
2025-12-17 22:36:46 +0300

    Сайдбар в процессе

--- components/layout/Menu/Menu.tsx
+++ components/layout/Menu/Menu.tsx
@@ -1,25 +1,92 @@
+import cn from 'classnames'
+
+import { FirstLevelMenuItem } from '@/interfaces/menu.interface'
 import { TopLevelCategory } from '@/interfaces/page.interface'
 import { getMenu } from '@/lib/menu'
 
+import CoursesIcon from './icons/courses-icon.svg'
+import ServicesIcon from './icons/services-icon.svg'
+import BooksIcon from './icons/books-icon.svg'
+import ProductsIcon from './icons/products-icon.svg'
+import styles from './Menu.module.css'
+
+const firstLevelMenu: FirstLevelMenuItem[] = [
+	{
+		route: 'courses',
+		name: 'Курсы',
+		icon: <CoursesIcon />,
+		id: TopLevelCategory.Courses,
+	},
+	{
+		route: 'services',
+		name: 'Сервисы',
+		icon: <ServicesIcon />,
+		id: TopLevelCategory.Services,
+	},
+	{
+		route: 'books',
+		name: 'Книги',
+		icon: <BooksIcon />,
+		id: TopLevelCategory.Books,
+	},
+	{
+		route: 'products',
+		name: 'Товары',
+		icon: <ProductsIcon />,
+		id: TopLevelCategory.Products,
+	},
+]
+
 export async function Menu() {
-	const firstCategory = TopLevelCategory.Products
-	console.log(firstCategory)
+	const firstCategory = TopLevelCategory.Courses
 	const menu = await getMenu(firstCategory)
 
-	console.log(menu)
-
-	return (
-		<div>
-			<ul>
-				{menu.map((el) => (
-					<li key={el._id.secondCategory}>
-						{el._id.secondCategory}
-						{el.pages.map((page) => (
-							<span>{page.category}</span>
-						))}
-					</li>
+	const renderFirstLevel = () => {
+		return (
+			<>
+				{firstLevelMenu.map((menuItem) => (
+					<div key={menuItem.route}>
+						<a href={`/${menuItem.route}`}>
+							<div
+								className={cn(styles.firstMenuItem, {
+									[styles.firstLevelItemActive]: menuItem.id === firstCategory,
+								})}
+							>
+								{menuItem.icon}
+								<span className={styles.menuItemName}>{menuItem.name}</span>
+							</div>
+						</a>
+						{menuItem.id === firstCategory && renderSecondLevel()}
+					</div>
 				))}
-			</ul>
-		</div>
-	)
+			</>
+		)
+	}
+
+	const renderSecondLevel = () => {
+		return (
+			<div className=''>
+				{menu.map((menuItem) => (
+					<div key={menuItem._id.secondCategory}>
+						<div className={styles.secondMenuItem}>
+							{menuItem._id.secondCategory}
+						</div>
+						<div
+							className={cn(styles.secondMenuItemTest, {
+								[styles.secondLevelItemOpened]: menuItem.isOpened,
+							})}
+						>
+							{renderThirdLevel()}
+						</div>
+					</div>
+				))}
+			</div>
+		)
+	}
+
+	const renderThirdLevel = () => {
+		return <> </>
+	}
+
+	return <div className={styles.menu}>{renderFirstLevel()}</div>
 }
--- interfaces/menu.interface.ts
+++ interfaces/menu.interface.ts
@@ -11,6 +11,7 @@ export interface PageItem {
 
 export interface MenuItem {
 	_id: { secondCategory: string }
+	isOpened?: boolean
 	pages: PageItem[]
 }
 
