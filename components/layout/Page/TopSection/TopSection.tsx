9209c75a1ad69a21b0194c2e9cf957cb5e7333f1
Фёдор Полевой <polevoy@ligat.su>
2025-12-26 15:40:50 +0300

    Работа над страницей (продолжаем)

--- components/layout/Page/TopSection/TopSection.tsx
+++ components/layout/Page/TopSection/TopSection.tsx
@@ -3,6 +3,8 @@ import { TopPageModel } from '@/interfaces/page.interface'
 import { ProductModel } from '@/interfaces/product.interface'
 import { Tag } from '@/components/ui/Tag/Tag'
 import { HhData } from '@/components/ui/HhData/HhData'
+import { Advantages } from '@/components/ui/Advantages/Advantages'
+import { Paragraph } from '@/components/ui/Paragraph/Paragraph'
 
 import styles from './TopSection.module.css'
 
@@ -45,10 +47,27 @@ export function TopSection({ page, products }: TopSectionProps) {
 					/>
 				)}
 				{page.advantages && page.advantages.length > 0 && (
-					<>
-						<Heading>Преимущества</Heading>
-					</>
+					<div>
+						<Heading tag='h2'>Преимущества</Heading>
+						<Advantages advantages={page.advantages} />
+					</div>
 				)}
+				{page.seoText && (
+					<div
+						className={styles.seoText}
+						dangerouslySetInnerHTML={{ __html: page.seoText }}
+					/>
+				)}
+				<div>
+					<Heading tag='h2'>Получаемые навыки</Heading>
+					<div className={styles.tagWrapper}>
+						{page.tags.map((t) => (
+							<Tag key={t} appearance='normal' color='primary'>
+								{t}
+							</Tag>
+						))}
+					</div>
+				</div>
 			</div>
 		</div>
 	)
---
+++ components/ui/Advantages/Advantages.tsx
@@ -0,0 +1,27 @@
+import { TopPageAdvantage } from '@/interfaces/page.interface'
+
+import CheckIcon from './check.svg'
+import styles from './Advantages.module.css'
+
+interface AdvantagesProps {
+	advantages: TopPageAdvantage[]
+	className?: string
+}
+
+export function Advantages({ advantages, className }: AdvantagesProps) {
+	console.log(advantages)
+	return (
+		<div className={styles.wrapper}>
+			{advantages.map((a) => (
+				<div key={a._id} className={styles.advantage}>
+					<div className={styles.icon}>
+						<CheckIcon />
+					</div>
+					<div className={styles.title}>{a.title}</div>
+					<hr className={styles.vLine} />
+					<div className={styles.description}>{a.description}</div>
+				</div>
+			))}
+		</div>
+	)
+}
