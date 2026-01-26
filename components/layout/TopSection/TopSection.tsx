71db69933e32fa9fc3700d0eacdc88253334e083
Фёдор Полевой <polevoy@ligat.su>
2026-01-26 16:59:54 +0300

    Фикс

--- components/layout/TopSection/TopSection.tsx
+++ components/layout/TopSection/TopSection.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ComponentPropsWithRef, useReducer } from 'react'
+import { useReducer } from 'react'
 import { motion } from 'framer-motion'
 
 import { Heading } from '@/components/ui/Heading/Heading'
