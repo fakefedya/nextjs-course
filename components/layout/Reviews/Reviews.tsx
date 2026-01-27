4b52d069c7b9bac2746af488b09320371d9e08a4
Фёдор Полевой <polevoy@ligat.su>
2026-01-27 10:06:32 +0300

    Анимируем отзывы

--- components/layout/Reviews/Reviews.tsx
+++ components/layout/Reviews/Reviews.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import cn from 'classnames'
+import { motion } from 'framer-motion'
 
 import { Card } from '@/components/ui/Card/Card'
 import { Button } from '@/components/ui/Button/Button'
@@ -21,6 +22,11 @@ interface ReviewsProps {
 export function Reviews({ productId, reviews }: ReviewsProps) {
 	const [isReviewsOpened, setIsReviewsOpened] = useState<boolean>(false)
 
+	const variants = {
+		visible: { opacity: 1, height: 'auto' },
+		hidden: { opacity: 0, height: 0 },
+	}
+
 	return (
 		<>
 			<div className={styles.actions}>
@@ -33,17 +39,18 @@ export function Reviews({ productId, reviews }: ReviewsProps) {
 					Читать отзывы
 				</Button>
 			</div>
-			<Card
-				className={cn(styles.reviews, {
-					[styles.opened]: isReviewsOpened,
-					[styles.closed]: !isReviewsOpened,
-				})}
+			<motion.div
+				animate={isReviewsOpened ? 'visible' : 'hidden'}
+				variants={variants}
+				initial='hidden'
 			>
-				{reviews.map((r) => (
-					<Review key={r._id} review={r} />
-				))}
-				<ReviewForm productId={productId} />
-			</Card>
+				<Card className={cn(styles.reviews)}>
+					{reviews.map((r) => (
+						<Review key={r._id} review={r} />
+					))}
+					<ReviewForm productId={productId} />
+				</Card>
+			</motion.div>
 		</>
 	)
 }
